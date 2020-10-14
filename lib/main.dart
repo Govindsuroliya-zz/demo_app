8b84fcb23476f74a743f53f408de6fc1074c9dd8
Govind Suroliya <govindsuroliya@outlook.com>
2020-10-14 09:57:57 +0530

    ch2 done

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Govind Suroliya"),
+      appBar: AppBar(
+        title: Text("Govind Suroliya"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        
+        child: Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    // color: Colors.teal,
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    // color: Colors.teal,
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    // color: Colors.teal,
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
-        body: Container(
-          child: Center(child: Text("HI Flutter")),
-        ),
+      ),
     );
   }
 }
