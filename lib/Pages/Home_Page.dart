2ae818571bad584547c9858e94058e521154eeab
Govind Suroliya <govindsuroliya@outlook.com>
2020-10-16 22:30:13 +0530

    ch4 done

--- lib/Pages/Home_Page.dart
+++ lib/Pages/Home_Page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import '../AppDrawer.dart';
-import '../Change_Name_Card.dart';
+import 'package:http/http.dart' as http;
+import 'package:demo_app/AppDrawer.dart';
+import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -13,9 +14,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _SomeController = TextEditingController();
   var textchange = "My Name";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  void getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
   }
 
   @override
@@ -26,12 +37,23 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: MyChangeNameCard(
-                textchange: textchange, SomeController: _SomeController),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      leading: Image.network(data[index]["url"]),
+                      subtitle: Text("ID : ${data[index]["id"]}"),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
