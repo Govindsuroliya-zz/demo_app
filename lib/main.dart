2fab8437eaa4fddd73417b5c6dd07f057de5359c
Govind Suroliya <govindsuroliya@outlook.com>
2020-10-24 16:56:07 +0530

    devbo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:demo_app/Pages/FaHoPa.dart';
+
 import 'package:demo_app/Pages/LoginScreen.dart';
 import 'package:demo_app/Ulitis/Constants.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'Pages/StHoPa.dart';
 
 
 Future main() async {
@@ -11,11 +13,11 @@ Future main() async {
   runApp(MaterialApp(
     title: "Demo App",
     debugShowCheckedModeBanner: false,
-    home: Constants.prefs.getBool("LoggedIn") == true ? HomePageFB():LoginPage(),
+    home: Constants.prefs.getBool("LoggedIn") == true ? HomePageSB():LoginPage(),
     theme: ThemeData(primarySwatch: Colors.red),
     routes: {
       "/login": (context) => LoginPage(),
-      "/home": (context) => HomePageFB(),
+      "/home": (context) => HomePageSB(),
     },
   ));
 }
